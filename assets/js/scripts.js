5e9363bfafbaeada8e789c8da53b0d7df91292eb
Alex Wallace <alexwallax@gmail.com>
2021-12-23 15:58:26 -0300

    Projeto js DIO

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -10,3 +10,21 @@ function decrement() {
     currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
 }
+
+var blocos = document.getElementsByClassName("bloco");
+
+for (let i = 0; i < blocos.length; i++) {
+    blocos[i].addEventListener("mouseover", aplicarEstilo); //quando o mouse passar por cima do elemento, aplica o estilo
+    blocos[i].addEventListener("mouseout", removerEstilo); //quando o mouse sai do elemento, aplica o estilo
+}
+
+function aplicarEstilo(obj) {
+    obj.target.style.border = "5px solid black";
+    obj.target.style.background = "Yellow";
+
+}
+
+function removerEstilo(obj) {
+    obj.target.style.border = "1px solid white";
+    obj.target.style.background = "Chocolate";
+}
